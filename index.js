5b5b38fbe645073a5625d621e06ddf71802f4ba1
edocz <noarch@163.com>
2017-07-09 13:38:11 +0800

    UPDATE

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const rss = require('rssspider');
+const fs  = require('fs');
 const cron= require('cron').CronJob;
+const moment = require('moment');
 const _   = require('lodash');
 const cheerio = require('cheerio');
+const DATAPATH= __dirname + '/data';
+
+var dataPool = [];
+var workPool = [];
 
 var tasks = [
 	{
 		name: 'reddit',
 		url : 'https://www.reddit.com/r/gif/.rss',
-		cron: '00 * * * * *',
+		cron: '00 */5 * * * *',
 		formater: function (records) {
 			var data = [];
 			_.each(records, function (record) {
@@ -25,7 +31,7 @@ var tasks = [
 	{
 		name: '4gifs',
 		url : 'http://tumblr.forgifs.com/rss',
-		cron: '00 * * * * *',
+		cron: '00 */5 * * * *',
 		formater: function (records) {
 			var data = [];
 			_.each(records, function (record) {
@@ -42,7 +48,7 @@ var tasks = [
 	{
 		name: '9gag',
 		url : 'http://9gag-rss.com/api/rss/get?code=9GAGGIF&format=2',
-		cron: '00 * * * * *',
+		cron: '00 */5 * * * *',
 		formater: function (records) {
 			var data = [];
 			_.each(records, function (record) {
@@ -59,9 +65,9 @@ var tasks = [
 	},
 ];
 
-var workPool = [];
-
 function initialize() {
+	if (!fs.existsSync(DATAPATH)) fs.mkdirSync(DATAPATH);
+	// add crawl task
 	_.each(tasks, function (task) {
 		var job = new cron({
 			cronTime: task.cron,
@@ -71,14 +77,28 @@ function initialize() {
 		});
 		workPool.push(job);
 	});
-}
 
+	var dumpJob = new cron({
+		cronTime: '59 59 23 * * *',
+		onTick: storeToFile,
+		start: true,
+		timeZone: 'Asia/Shanghai'
+	});
+}
+// job handler
 function cronTick(task) {
-	rss.fetchRss(task.url, ['title', 'description']).then(task.formater).then(save);
+	rss.fetchRss(task.url, ['title', 'description']).then(task.formater).then(cache);
+}
+// cache temp data to dataPool
+function cache(data) {
+	dataPool = _.union(dataPool, data)
 }
 
-function save(data) {
-	console.log(data);
+function storeToFile() {
+	var today = moment().format('YYYYMMDD');
+	var file  = `${DATAPATH}/${today}.json`;
+	fs.writeFileSync(file, JSON.stringify(dataPool));
+	dataPool = [];
 }
 
 initialize();
